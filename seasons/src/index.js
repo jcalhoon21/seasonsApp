bfe91127bb34df7082346b16ba7f67a8be9ea335
Jennifer Calhoon <jennifer.calhoon@interpreta.com>
2020-03-23 12:29:43 -0700

    Setting state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position, 'position'),
-    (err) => console.log(err)
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props); // reference to React.Component function
 
+    this.state ={
+      latitude: null, // we dont know what it is yet but eventually we will
+      errorMessage: ''
+    };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        //setState updates state // never do a direct state assignment -- no this.state.latitude = ...
+        this.setState({ latitude: position.coords.latitude });
+      },
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    
   return (
     <div>
-    empty  
+      Latitude: {this.state.latitude} <br />
+      Error: {this.state.errorMessage}
     </div>
   )
+}
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
