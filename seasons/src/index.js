28262e375f38e50f798b5203f2e6c711e2130492
Jennifer Calhoon <jennifer.calhoon@interpreta.com>
2020-03-23 13:20:15 -0700

    adding state through props

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props); // reference to React.Component function
+  // constructor(props) {
+  //   super(props); // reference to React.Component function
 
-    this.state ={
-      latitude: null, // we dont know what it is yet but eventually we will
-      errorMessage: ''
-    };
+  //   this.state ={
+  //     latitude: null, // we dont know what it is yet but eventually we will
+  //     errorMessage: ''
+  //   };
+  // }
 
+  // same as constructor
+  state = { latitude: null, errorMessage: ''};
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        //setState updates state // never do a direct state assignment -- no this.state.latitude = ...
-        this.setState({ latitude: position.coords.latitude });
-      },
+      (position) => this.setState({ latitude: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     );
   }
@@ -26,18 +29,19 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.latitude) {
-      return <div>Latitude: {this.state.latitude}</div>
+      // return <div>Latitude: {this.state.latitude}</div>
+      return <div><SeasonDisplay latitude={this.state.latitude} /></div>
     }
 
     return <div>Loading...</div>
-    
+
   // return (
   //   <div>
   //     Latitude: {this.state.latitude} <br />
   //     Error: {this.state.errorMessage}
   //   </div>
   // )
-}
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
