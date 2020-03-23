cab44cefb236a94aa7e5a230079cb4e5ce476958
Jennifer Calhoon <jennifer.calhoon@interpreta.com>
2020-03-23 13:48:34 -0700

    Season Display component and ternary text

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if(month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat < 0 ? 'winter' : 'summer';
+  }
+}
+
 const SeasonDisplay = (props) => {
-  console.log(props.latitude)
+  // console.log(props.latitude)
+  const season = getSeason(props.latitude, new Date().getMonth());
+  // console.log(season);
+  const text = season === 'winter' ? 'Burrr it\'s chilly!' : 'Let\'s hit the beach!';
+
   return (
     <div>
-      Season Display
+      <h1>{text}</h1>
     </div>
   )
 }
