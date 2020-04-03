b8b221ea17b84a62487645ad4b26bf8e7d05e14a
jcalhoon21 <jcalhoon21@gmail.com>
2020-04-03 16:08:09 -0700

    Final Commit

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+//Functional Component
+// const App = () => {
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position, 'position'),
+//     (err) => console.log(err)
+//   );
+
+//   return (
+//     <div>
+//       Latitude: 
+//     </div>
+//   )
+// };
+
+
+//Class Based Component
+class App extends React.Component {
+    
+  render() {
   window.navigator.geolocation.getCurrentPosition(
     (position) => console.log(position, 'position'),
     (err) => console.log(err)
-  );
-
-  return (
-    <div>
-    empty  
-    </div>
-  )
+    );
+    
+  return <div> Latitude: </div>
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
